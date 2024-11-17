4b4e566dbf6eb887668f8a87e55f1f4c91803ec0
Pravesh Kumar <161683443+Pravesh2201@users.noreply.github.com>
2024-11-17 16:26:02 +0530

    Update EmployeeList.js

--- frontend/src/EmployeeList.js
+++ frontend/src/EmployeeList.js
@@ -10,7 +10,7 @@ class ListEmployee extends React.Component {
 	}
 	
 	loadData() {
-		fetch("http://15.206.213.8:8080/api/v1/employee/search/all")
+		fetch("http://52.200.226.197:8082/api/v1/employee/search/all")
 			.then(response => response.json())
 			.then(data => {
 				this.setState({data: data })
