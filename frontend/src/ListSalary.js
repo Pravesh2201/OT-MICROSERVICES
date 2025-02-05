d80e2e2c86e288697023c0af7025510b485741b0
Pravesh Kumar <161683443+Pravesh2201@users.noreply.github.com>
2025-02-05 20:33:31 +0530

    Update ListSalary.js

--- frontend/src/ListSalary.js
+++ frontend/src/ListSalary.js
@@ -1,25 +1,83 @@
+// import react, * as React from "react";
+// import { Page, Grid, Table, Button } from "tabler-react";
+// import SiteWrapper from "./SiteWrapper.react";
+
+// class ListSalary extends React.Component {
+// 	constructor(props) {
+// 		super(props)
+// 		this.state = { data: [] }
+// 	}
+
+// 	loadData() {
+// 		fetch('http://98.85.32.90:8080/api/v1/salary/search/all')
+// 			.then(response => response.json())
+// 			.then(data => {
+// 				this.setState({data: data })
+// 		})
+// 			.catch(err => console.error(this.props.url, err.toString()))
+// 	}
+
+// 	componentDidMount() {
+// 		this.loadData()
+// 	}
+
+//   render() {
+//       return (
+//           <SiteWrapper>
+//           <Page.Card
+//               title="Salary List"
+//           ></Page.Card>
+//           <Grid.Col md={6} lg={10} className="align-self-center">
+//           <Table>
+//             <Table.Header>
+//                  <Table.ColHeader>Employee ID</Table.ColHeader>
+//                  <Table.ColHeader>Name</Table.ColHeader>
+//                  <Table.ColHeader>Salary</Table.ColHeader>
+//             </Table.Header>
+//             <Table.Body>
+//            { this.state.data.map((item, i) => {
+//                 return (
+//                     <Table.Row>
+//                         <Table.Col>{item.id}</Table.Col>
+//                         <Table.Col>{item.name}</Table.Col>
+//                         <Table.Col>{item.annual_package}</Table.Col>
+//                     </Table.Row>
+//                 );
+//                 })
+//             }
+//             </Table.Body>
+//             </Table>
+//           </Grid.Col>
+//           </SiteWrapper>
+//       );
+//   }
+// }
+// //
+// export default ListSalary
+// //export default withTransaction('ListSalary', 'component')(ListSalary)
+
 import react, * as React from "react";
 import { Page, Grid, Table, Button } from "tabler-react";
 import SiteWrapper from "./SiteWrapper.react";
 
 class ListSalary extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = { data: [] }
-	}
+        constructor(props) {
+                super(props)
+                this.state = { data: [] }
+        }
 
-	loadData() {
-		fetch('http://98.85.32.90:8080/api/v1/salary/search/all')
-			.then(response => response.json())
-			.then(data => {
-				this.setState({data: data })
-		})
-			.catch(err => console.error(this.props.url, err.toString()))
-	}
+        loadData() {
+                fetch('http://54.179.28.99:8080/api/v1/salary/search/all')
+                        .then(response => response.json())
+                        .then(data => {
+                                this.setState({data: data })
+                })
+                        .catch(err => console.error(this.props.url, err.toString()))
+        }
 
-	componentDidMount() {
-		this.loadData()
-	}
+        componentDidMount() {
+                this.loadData()
+        }
 
   render() {
       return (
@@ -40,7 +98,7 @@ class ListSalary extends React.Component {
                     <Table.Row>
                         <Table.Col>{item.id}</Table.Col>
                         <Table.Col>{item.name}</Table.Col>
-                        <Table.Col>{item.annual_package}</Table.Col>
+                        <Table.Col>{item.salary}</Table.Col>
                     </Table.Row>
                 );
                 })
